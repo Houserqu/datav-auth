d74bf4c07bb9a1441cfad2e3ea8bb4f5871ec7a4
houser <houserqu@qq.com>
2019-02-27 15:45:15 +0800

    feat：根据userid获取权限

--- app/router.ts
+++ app/router.ts
@@ -5,12 +5,16 @@ export default (app: Application) => {
 
   router.get('/', controller.home.index);
 
-  // 角色管理
-  router.resources('roles', '/api/auth/roles', controller.roles);
+  // user 权限
+  router.get('/api/auth/user/:id', controller.user.userPermission)
 
   // 角色权限管理
   router.get('/api/auth/roles/:id/permissions', controller.roles.rolePermissions);
   router.post('/api/auth/roles/:id/permissions', controller.roles.updateRolePermissions);
+
+  // 角色管理
+  router.resources('roles', '/api/auth/roles', controller.roles);
+
 
   // 权限管理
   router.resources('permissions', '/api/auth/permissions', controller.permissions);
--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -5,11 +5,13 @@ import 'egg';
 import ExportHome from '../../../app/controller/home';
 import ExportPermissions from '../../../app/controller/permissions';
 import ExportRoles from '../../../app/controller/roles';
+import ExportUser from '../../../app/controller/user';
 
 declare module 'egg' {
   interface IController {
     home: ExportHome;
     permissions: ExportPermissions;
     roles: ExportRoles;
+    user: ExportUser;
   }
 }
